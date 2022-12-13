4ce51168f0825149b34f0220a6b9edf2ed18ed25
Denchik1307 <87667449+Denchik1307@users.noreply.github.com>
2022-12-13 10:02:02 +0300

    Update first_homework.py
    some fix

--- PracticeOne/first_homework.py
+++ PracticeOne/first_homework.py
@@ -2,9 +2,20 @@ import os
 os.system('cls' if os.name == 'nt' else 'clear')
 
 
+def getNumericFromConsole(text):
+    while True:
+        try:
+            res = int(input(text))
+            break
+        except ValueError:
+            print("Numbers only please!")
+    return res
+
+
 def SummDigitNum():
     result = 0
-    num = input("input triple number: ")
+    num = getNumericFromConsole("input triple number: ")
+    num = str(num)
     if len(num) != 3:
         print("wrong input!!!")
     else:
@@ -17,7 +28,8 @@ def LuckyTicket():
     firstHalf = 0
     secondHalf = 0
     isLucky = False
-    ticket = input("input number ticket: ")
+    ticket = getNumericFromConsole("input number ticket: ")
+    ticket = str(ticket)
     if len(ticket) != 6 and int(ticket):
         print("you made a mistake!!!")
     else:
@@ -31,18 +43,22 @@ def LuckyTicket():
 
 
 def Aists():
-    allAists = int(input("input all aists: "))
+    allAists = getNumericFromConsole("input all aists: ")
     katy = int((allAists/3)*2)
     petya = int((katy/2)/2)
     sergey = int(petya)
-    print("\nPetya", petya, "\nKaty", katy, "\nSergey", sergey)
-    
+    if allAists % 6 == 0:
+        print("\nPetya", petya, "\nKaty", katy, "\nSergey", sergey)
+    else:
+        print("wrong quantity aists")
+
+
 def ChokolateCut():
     print("input size chocolate N x M : ")
-    n = int(input("inut N: "))
-    m = int(input("inut M: "))
-    k = int(input("input K: "))
-    if k%n or k%m:
+    n = getNumericFromConsole("inut N: ")
+    m = getNumericFromConsole("inut M: ")
+    k = getNumericFromConsole("input K: ")
+    if k % n or k % m:
         print("Yes")
     else:
         print("No")
@@ -61,4 +77,4 @@ def ExserciseSelect(num):
         print("input error")
 
 
-ExserciseSelect(int(input("input number exercise (2,4,6 or 8)")))
+ExserciseSelect(getNumericFromConsole("input number exercise (2,4,6 or 8)"))
