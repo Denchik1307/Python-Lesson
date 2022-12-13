702bd5a2d2a8761aceabddd961ab4ed26824286a
Denis <denis13071981@gmail.com>
2022-12-13 21:34:41 +0300

    lesson

--- for_lesson/lesson_one.py
+++ for_lesson/lesson_one.py
@@ -1,16 +1,13 @@
 import os
 os.system('cls' if os.name == 'nt' else 'clear')
 
-
 def Distance():
     n = 700
     m = 701
     result = (m + n - 1) // n
     print(result)
 
-
 Distance()
-
 
 def CountTable():
     classA = int(input("Кол-во учеников класса A"))
@@ -19,9 +16,7 @@ def CountTable():
     result = (classA + classB + classC + 1) // 2
     print(result)
 
-
 CountTable()
-
 
 def Locomotive():
     i = int(input("порядковый номер вагона: "))
@@ -29,16 +24,14 @@ def Locomotive():
     result = i + j - 1
     print(result, "вагонов")
 
-
 Locomotive()
 
-
-def IsYear():
+def IsHighYear():
     i = int(input("Введите год: "))
     if (i % 4 == 0 and i % 100 != 0) or (i % 400 == 0):
         print('YES')
     else:
         print('NO')
 
+IsHighYear()
 
-IsYear()
