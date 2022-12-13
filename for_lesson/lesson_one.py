56c37037c887c3e42eb9089a0d5c6d923d5061ce
Denis <denis13071981@gmail.com>
2022-12-13 19:20:42 +0300

    lesson one

---
+++ for_lesson/lesson_one.py
@@ -0,0 +1,44 @@
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def Distance():
+    n = 700
+    m = 701
+    result = (m + n - 1) // n
+    print(result)
+
+
+Distance()
+
+
+def CountTable():
+    classA = int(input("Кол-во учеников класса A"))
+    classB = int(input("Кол-во учеников класса Б"))
+    classC = int(input("Кол-во учеников класса B"))
+    result = (classA + classB + classC + 1) // 2
+    print(result)
+
+
+CountTable()
+
+
+def Locomotive():
+    i = int(input("порядковый номер вагона: "))
+    j = int(input("номер на вагоне: "))
+    result = i + j - 1
+    print(result, "вагонов")
+
+
+Locomotive()
+
+
+def IsYear():
+    i = int(input("Введите год: "))
+    if (i % 4 == 0 and i % 100 != 0) or (i % 400 == 0):
+        print('YES')
+    else:
+        print('NO')
+
+
+IsYear()
