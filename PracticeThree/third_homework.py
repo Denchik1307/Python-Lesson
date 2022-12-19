f8e23fc76ee33e706a7fe75b5272b83f612e3bc1
Denis <denis13071981@gmail.com>
2022-12-20 00:22:13 +0300

    third homework

--- PracticeThree/third_homework.py
+++ PracticeThree/third_homework.py
@@ -37,8 +37,14 @@ def CountNumber():
 
 def Scrabble():
     word = input("Input your word: ")
-    lib = {"AEIOULNSTRАВЕИНОРСТ": 1, "DGДКЛМПУ": 2, "BCMPБГЁЬЯ": 3,
-           "FHVWYЙЫ": 4, "KЖЗХЦЧ": 5, "JXШЭЮ": 8, "QZФЩЪ": 10}
+    lib = \
+    {"AEIOULNSTRАВЕИНОРСТ": 1,
+    "DGДКЛМПУ": 2,
+    "BCMPБГЁЬЯ": 3,
+    "FHVWYЙЫ": 4,
+    "KЖЗХЦЧ": 5,
+    "JXШЭЮ": 8,
+    "QZФЩЪ": 10}
     count = 0
     for i in word:
         for val, key in enumerate(lib.keys()):
