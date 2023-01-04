94c64be52e95337b6b4d8506c11caa4f7337b878
Denis <denis13071981@gmail.com>
2023-01-04 23:16:48 +0300

    from pcharm

--- DopOne/dop_one.py
+++ DopOne/dop_one.py
@@ -1,15 +1,18 @@
-import os
 import math
-import re
+import click
+import os
 from random import randint
 
-def Clear() -> None: os.system('cls')
 
-def EndExercise() -> None:
+def clear() -> None: os.system('cls')
+
+
+def end_exercise() -> None:
     input("\npress enter to continue")
-    Clear()
+    clear()
 
-def GetPositiveNumericFromConsole(text="") -> int:
+
+def get_positive_numeric_from_console(text="") -> int:
     res = -1
     while res < 0:
         try:
@@ -22,202 +25,213 @@ def GetPositiveNumericFromConsole(text="") -> int:
             print("Numbers only please!")
     return res
 
-def GetNumericFromConsole(text="") -> int:
+
+def get_numeric_from_console(text="") -> int:
     while True:
         try:
             res = int(input(text))
             break
         except ValueError:
-            Clear()
+            clear()
             print("Numbers only please!")
     return res
 
-def CreateRandomList(min: int, max: int, size: int):
-    return [randint(min, max + 1) for x in range(size)]
 
-def IsPrime(num) -> bool:
+def create_random_list(minimal: int, maximal: int, size: int) -> list[int]:
+    return [randint(minimal, maximal + 1) for _ in range(size)]
+
+
+def is_prime(num) -> bool:
     for i in range(2, num >> 1):
         if num % i == 0:
             return False
     return True
 
-def Exercise101() -> None:
+
+def exercise101() -> None:
     num = 0.001
     pi = math.pi
     tmp = str(num).split(".")
     print(round(pi, len(tmp[1])))
-    EndExercise()
+    end_exercise()
 
-def WriteFile(file: str, text: str) -> None:
+
+def write_file(file: str, text: str) -> None:
     with open(file, "w") as file:
         file.write(text)
 
-def Exercise102() -> None:
-    n = GetNumericFromConsole("Insert number: ")
-    print([num for num in range(1, n + 1) if n % num == 0], "все целые")
-    print([num for num in range(1, n + 1) if n %
-          num == 0 and IsPrime(num)], "простые")
-    EndExercise()
 
-def Exercise103() -> None:
-    def MakeFormula(coeff: int) -> str:
-        listNum = CreateRandomList(0, 100, coeff)
-        sizeList = len(listNum)-1
+def exercise102() -> None:
+    n = get_numeric_from_console("Insert number: ")
+    print([num for num in range(1, n + 1) if n % num == 0], "all digit")
+    print([num for num in range(1, n + 1) if n %
+           num == 0 and is_prime(num)], "simple")
+    end_exercise()
+
+
+def exercise103() -> None:
+    def make_formula(coefficient: int) -> str:
+        list_num = create_random_list(0, 100, coefficient)
+        size_list = len(list_num) - 1
         result = ''
-        for i in listNum:
+        for i in list_num:
             tmp = str(i) if 1 != i != 0 else ""
-            if sizeList > 1:
-                result += f"{tmp}x**{sizeList} + "
-            elif sizeList == 1:
+            if size_list > 1:
+                result += f"{tmp}x**{size_list} + "
+            elif size_list == 1:
                 result += f"{tmp}x + "
-            elif sizeList == 0:
+            elif size_list == 0:
                 if i == 0:
                     result = result[:-3]
                     break
                 else:
                     result += tmp
-            sizeList -= 1
+            size_list -= 1
         result += " = 0"
         return result
 
-    coeffOne = GetPositiveNumericFromConsole(
+    coefficient_one = get_positive_numeric_from_console(
         "Input coefficient for file one: ")
-    coeffTwo = GetPositiveNumericFromConsole(
+    coefficient_two = get_positive_numeric_from_console(
         "Input coefficient for file two: ")
 
-    WriteFile("104_file1.txt", MakeFormula(coeffOne))
-    WriteFile("104_file2.txt", MakeFormula(coeffTwo))
+    write_file("104_file1.txt", make_formula(coefficient_one))
+    write_file("104_file2.txt", make_formula(coefficient_two))
     print("File created")
-    EndExercise()
+    end_exercise()
 
-def Exercise104() -> None:
 
-    def FunToDict(file: str) -> dict:
+def exercise104() -> None:
+    def fun_to_dict(file: str) -> dict:
         with open(file, 'r') as file:
             poly = file.readline()
-            polyReplased = poly.replace(" ", "").replace(
+            poly_replaced = poly.replace(" ", "").replace(
                 "=0", "").replace("\n", "").split("+")
 
-        resDict = {}
-        for i in polyReplased:
+        res_dict = {}
+        for i in poly_replaced:
             if "x**" in i:
-                resDict[i[i.find("x")+3]] = i[:i.find("*")-1]
+                res_dict[i[i.find("x") + 3]] = i[:i.find("*") - 1]
             elif "x" in i:
-                resDict[1] = i[:i.find("x")]
+                res_dict[1] = i[:i.find("x")]
             elif "x" not in i:
-                resDict[0] = i
-        return resDict
+                res_dict[0] = i
+        return res_dict
 
-    dicOne = FunToDict('104_file1.txt')
-    dicTwo = FunToDict('104_file2.txt')
+    dic_one = fun_to_dict('104_file1.txt')
+    dic_two = fun_to_dict('104_file2.txt')
     res = ""
-    tmp = ""
-    for key in dicOne:
+    for key in dic_one:
         try:
-            tmp = dicTwo[key]
-        except:
+            tmp = dic_two[key]
+        except IndexError as e:
+            print(e)
             tmp = "0"
 
-        sum = int(dicOne[key]) - int(tmp)
+        summa = int(dic_one[key]) - int(tmp)
         deg = f"x**{key}" if int(key) >= 2 else "x" if int(key) == 1 else ""
-        znak = " + " if sum >= 0 else " - "
-        sum = sum if sum > 0 else sum * -1
-        res += f"{znak}{sum}{deg}"
+        plus_or_minus = " + " if summa >= 0 else " - "
+        summa = summa if summa > 0 else summa * -1
+        res += f"{plus_or_minus}{summa}{deg}"
 
     res += " = 0"
-    WriteFile("104_result.txt",res)
+    write_file("104_result.txt", res)
     print("File created")
-    EndExercise()
+    end_exercise()
 
-def Exercise105() -> None:
-    print([res for res in input(
-        """\"\"\"Напишите программу, удаляющую из текста все слова,
-содержащие "абв" \"\"\"
-введите слова разделяя пробелом: """).split() if "абв" not in res])
-    EndExercise()
 
-def Exercise106() -> None:
-    maxTakeCandys = 28
-    candys: int = 2021
-    print(f"There {candys} candys")
-    isPlayerOne = True
-    selectPlayer = input("Input 1 or Yes for play with BOT: ").lower()
-    isBot = True if (selectPlayer == "yes" or selectPlayer == "1") else False
-    if isBot:
+def exercise105() -> None:
+    print([res for res in input("Input letters separate with space: ")
+          .split() if "абв" not in res])
+    end_exercise()
+
+
+def exercise106() -> None:
+    max_take_candy = 28
+    candy: int = 2021
+    print(f"There {candy} candies")
+    is_player_one = True
+    level = 0
+    select_player = input("Input 1 or Yes for play with BOT: ").lower()
+    is_bot = True if (select_player == "yes" or select_player == "1") else False
+    if is_bot:
         while True:
-            level = GetPositiveNumericFromConsole("Select level (low - hard -> 0 -> 3: ")
+            level = get_positive_numeric_from_console("Select level (low - hard -> 0 -> 3: ")
             if 0 <= level <= 3:
                 break
             else:
                 print("only number 0-3")
         level *= 3
-    playerOne = input("Input name first player: ")
-    playerTwo = "BOT" if isBot else input("Input name second player: ")
+    player_one = input("Input name first player: ")
+    player_two = "BOT" if is_bot else input("Input name second player: ")
 
-    def GetCandys(player: str) -> int:
-        candy = -1
-        while (True):
-            print(msg := "You need to take from 1 to 28 pcs")
-            candy = GetPositiveNumericFromConsole(f"Take candys ({player}): ")
-            if 28 >= candy >= 1:
+    def get_candy(player: str) -> int:
+        while True:
+            print("You need to take from 1 to 28 pcs")
+            pcs_candies = get_positive_numeric_from_console(f"Take candies ({player}): ")
+            if 28 >= pcs_candies >= 1:
                 break
-        return candy
+        return pcs_candies
 
-    def Play(candys: int, takePCS: int) -> int:
-        candys = candys - takePCS
-        return candys
+    def play(pcs: int, take_pcs: int) -> int:
+        pcs = pcs - take_pcs
+        return pcs
 
-    while (True):
-        message = f"Player {playerOne}" if isPlayerOne else f"Player {playerTwo}"
-        if candys > maxTakeCandys:
+    while True:
+        message = f"Player {player_one}" if is_player_one else f"Player {player_two}"
+        if candy > max_take_candy:
             print()
-            if isBot and not isPlayerOne:
-                rand = candys%maxTakeCandys + level + randint(0, level)
-                candys = Play(candys, rand)
-                Clear()
-                print(f"BOT takes {rand} candys")
+            if is_bot and not is_player_one:
+                rand = candy % max_take_candy + level + randint(0, level)
+                candies = play(candy, rand)
+                clear()
+                print(f"BOT takes {rand} candies")
             else:
-                candys = Play(candys, GetCandys(message))
-            print(f"there are {candys} candies left")
+                candies = play(candy, get_candy(message))
+            print(f"there are {candies} candies left")
         else:
             print(message, "WIN")
             break
-        isPlayerOne = not isPlayerOne
-    EndExercise()
+        is_player_one = not is_player_one
+    end_exercise()
 
-def Exercise107() -> None:
+
+def exercise107() -> None:
     print(107)
-    EndExercise()
+    end_exercise()
 
-def Exercise108() -> None:
+
+def exercise108() -> None:
     print(108)
-    EndExercise()
+    end_exercise()
 
-def ExserciseSelect() -> None:
+
+def exercise_select() -> None:
     while True:
-        num = GetPositiveNumericFromConsole(
+        num = get_positive_numeric_from_console(
             "input number exercise (101, 102, 103, 104, 105, 106, 107 or 108) or 0 to exit: ")
-        Clear()
+        clear()
         if num == 101:
-            Exercise101()
+            exercise101()
         elif num == 102:
-            Exercise102()
+            exercise102()
         elif num == 103:
-            Exercise103()
+            exercise103()
         elif num == 104:
-            Exercise104()
+            exercise104()
         elif num == 105:
-            Exercise105()
+            exercise105()
         elif num == 106:
-            Exercise106()
+            exercise106()
         elif num == 107:
-            Exercise107()
+            exercise107()
         elif num == 108:
-            Exercise108()
+            exercise108()
         elif num == 0:
             break
         else:
             print("input error")
-    Clear()
+    clear()
 
-ExserciseSelect()
+
+exercise_select()
