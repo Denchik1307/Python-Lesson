5d740ef1a030740701a0e0107b82521ea8fac999
Denis <denis13071981@gmail.com>
2023-01-12 23:16:16 +0300

    remove some comments

--- PracticeFive/five_homework.py
+++ PracticeFive/five_homework.py
@@ -36,8 +36,6 @@ def exercise_28():
         if b == 0:
             return a
         else:
-            # b -= 1
-            # a += 1
             return my_summa(a + 1, b - 1)
 
     print(my_summa(first_num, second_num))
